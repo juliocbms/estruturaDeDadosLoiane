4a995d1c28358b84764b542d3ff9f5e4e2de3fb9
julio <juliocesar13b@gmail.com>
2025-11-04 20:19:24 -0300

    commit

--- estruturaDeDados/src/teste/Aula02.java
+++ estruturaDeDados/src/teste/Aula02.java
@@ -1,4 +1,12 @@
 package teste;
 
+import vetor.Vetor;
+
 public class Aula02 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(100);
+
+        int i = 0;
+        i++;
+    }
 }
